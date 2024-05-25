db3da482277e2307bc8885e524d002cc27a99ff4
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-05-25 17:02:54 +0500

    Added the main menu

---
+++ program.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int choice;
+
+    while (true)
+    {
+        cout << "RestroXpert - Main Menu" << endl;
+        cout << "1. Admin Panel" << endl;
+        cout << "2. Customer Panel" << endl;
+        cout << "3. Staff Attendance" << endl;
+        cout << "4. Feedback Panel" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            // Admin Panel code here
+            break;
+        case 2:
+            // Customer Panel code here
+            break;
+        case 3:
+            // Staff Attendance code here
+            break;
+        case 4:
+            // Feedback Panel code here
+            break;
+        case 0:
+            cout << "Exiting RestroXpert. Goodbye!" << endl;
+            return 0;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
